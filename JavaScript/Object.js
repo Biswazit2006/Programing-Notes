1a8774f75ce8f2c36987cc6d87548fd7b4e0d63c
Biswazit <biswasbiswazit@gmail.com>
2025-08-17 23:51:58 +0600

    updated object

--- JavaScript/Object.js
+++ JavaScript/Object.js
@@ -1,10 +1,11 @@
-// JavaScript Object
+//      ***JavaScript Object**
 const Mobile = {
-  // Key:value
+  // Key:value,
   brand: "Samsung",
   model: "S24 ultra",
   variant: "Global",
   processor: "Snapdragon",
+  //  Arrey in object
   camera: ["200mp", "12mp", "12mp"],
   telephoto: true,
   "selfie camera": "12mp",
@@ -13,22 +14,24 @@ const Mobile = {
     return `${this.model} ${this.variant}`;
   },
 };
-Mobile.model = "S25 Ultra"; //Update element in object
-console.log(Mobile); //Print the object
 
-// print indivitual key
-console.log(Mobile.brand);
-console.log(Mobile["selfie camera"]);
+// console.log(Mobile);             //print the object
+Mobile.model = "S25 Ultra";         //Update element in object
+console.log(Mobile);                //Print the updated object
 
-console.log(Mobile.hasOwnProperty("camera")); //To cheak existance of a elements
+//      **print indivitual key**
+// console.log(Mobile.brand);              //F1
+// console.log(Mobile["selfie camera"]);   //F2
 
-// Convert object to Arrays
-console.log(Object.keys(Mobile)); //Keys Arrey
-console.log(Object.values(Mobile)); //Keys Values
+// console.log(Mobile.hasOwnProperty("camera"));   //To cheak existance of a elements
 
-// Fredge object
-Object.freeze(Mobile); //For fredge
-Mobile.model = "S26 Ultra"; //It will not update
+//      **Convert object to Arrays**
+// console.log(Object.keys(Mobile));       //Keys Arrey
+// console.log(Object.values(Mobile));     //Keys Values
+
+//      **Fredge object**
+// Object.freeze(Mobile);              //For fredge
+// Mobile.model = "S26 Ultra";         //It will not update
 // console.log(Mobile);
 
 const obj1 = {
@@ -47,25 +50,26 @@ const obj3 = {
   Z: 33,
 };
 
-// Margeing Object
-let fobj = Object.assign({}, obj1, obj2, obj3); //F1 Marged
-console.log(fobj); //Print Marged Object
-let fobj2 = Object.assign(obj1, obj2, obj3); //F2 Marged
-console.log(fobj2); //Print Marged Object
+//      **Margeing Object**
+let fobj = Object.assign({}, obj1, obj2, obj3);   //F1 Marged
+// console.log(fobj);                                //Print Marged Object
+let fobj2 = Object.assign(obj1, obj2, obj3);      //F2 Marged
+// console.log(fobj2);                               //Print Marged Object
 
 
 
-// Constractor Object
+//      **Constractor Object**
 function Person(fname, lname) {
   (this.firstName = fname), (this.lastName = lname);
 }
 // Creat different people
-let person1 = new Person("ashok", "kumer"); //Person1
-console.log(person1); //Print person1
-let person2 = new Person("Rohit", "Sharma"); //Person2
-person2.lastName = "kumer"; //Update Persons elemente
-person2.age = 25; //Add new element in Person
-console.log(person2); //Print person2
+let person1 = new Person("ashok", "kumer");     //Person1
+// console.log(person1);                           //Print person1
+
+let person2 = new Person("Rohit", "Sharma");    //Person2
+person2.lastName = "kumer";                     //Update Persons elemente
+person2.age = 25;                               //Add new element in Person
+// console.log(person2);                           //Print person2
 
 /*
 We can take object in 2 ways.
@@ -75,3 +79,4 @@ We can Access object in 2 ways
   a)dot notetion
   b)bracket notetion
 */
+ 
